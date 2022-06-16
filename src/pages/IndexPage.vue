04787f79971b3d489c4b71a96388d8c3a47bb77f
stha <stha@aprixon.de>
2022-06-16 19:16:15 +0200

    add counter to page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,14 +2,22 @@
   <q-page padding>
     <h4>Pinia-Demo</h4>
     <div class="row q-gutter-sm q-mt-sm">
-
+      <q-input type="number" v-model.number="counterStore.counter"></q-input>
+      <q-btn @click="counterStore.counter++">add 1</q-btn>
     </div>
+      <p>{{counterStore.counter}}</p>
   </q-page>
 </template>
 
 <script>
+import {useCounterStore} from "stores/CounterStore";
 
 export default {
-  //
+  setup() {
+    return {
+      counterStore: useCounterStore()
+    }
+  }
+  // data, methods, computed, etc.
 }
 </script>
